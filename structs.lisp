ed2a2c176c53a74ee7fe670774df9a336afb8945
Eitr <Ginnungagap@rocketmail.com>
2013-02-19 09:24:09 -0800

    Create structs.lisp
    Data structures definition

---
+++ structs.lisp
@@ -0,0 +1,23 @@
+(in-package "ACL2")
+(include-book "world" :dir :teachpacks)
+
+(defstructure card
+   (suit  		(:assert (integerp suit)))
+   (value		(:assert (integerp value))))
+
+(defstructure hand
+   (cards 		(:assert (listp cards)))) 
+
+(defstructure player
+  (name			(:assert (stringp name)))
+  (chips 		(:assert (integerp chips)))
+  (call-amount		(:assert (integerp call-amount)))
+  (ready 		(:assert (booleanp ready)))
+  (cards			(:assert (hand-p cards))))
+
+(defstructure gamestate
+   (players		(:assert (listp players)))
+   (common		(:assert (hand-p common)))
+   (last-raise		(:assert (player-p last-raise)))
+   (seed			(:assert (integerp seed)))
+   (pot			(:assert (integerp pot))))
