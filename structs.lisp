81c17a579da1669540e450c677f227071f3434a7
Eitr <Ginnungagap@rocketmail.com>
2013-03-05 10:57:41 -0600

    Update structs.lisp
    Spacing

--- structs.lisp
+++ structs.lisp
@@ -3,10 +3,10 @@
 
 ; added by Tom - feel free to reformat and remove this comment, just let me know if anything important changes
 (defstructure request
-   (type (:assert (stringp type)))
-   (player (:assert (stringp player)))
-   (bet (:assert (integerp bet)))
-   (ready (:assert (booleanp ready))))
+   (type       (:assert (stringp  type)))
+   (player     (:assert (stringp  player)))
+   (bet        (:assert (integerp bet)))
+   (ready      (:assert (booleanp ready))))
 
 (defstructure card
    (suit  		(:assert (integerp suit)))
