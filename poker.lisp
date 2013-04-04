e0e1c4fc7b0a6d86835506d316f7e8e4c0307650
Tom Lindley <happycocoanuts@gmail.com>
2013-04-04 09:13:08 -0500

    Implemented getHandRank() and modified quickSort() to sort hands.

--- poker.lisp
+++ poker.lisp
@@ -3,61 +3,73 @@
 (include-book "structs")
 (include-book "cards")
 
-(defun handQuickSortHelper (h lesser greater pivot value)
+(defun quickSortHelper (h lesser greater pivot value)
   (if (not(consp h))
       (list lesser greater)
       (let ((front (car h)))
-        (if (equal value "rank")
-            (if (<= (card-rank front) (card-rank pivot)) 
-                (handQuickSortHelper (cdr h) 
+        (if (equal value "value")
+            (if (<= (card-value front) (card-value pivot))
+                (quickSortHelper (cdr h) 
                                      (cons front lesser) 
                                      greater 
                                      pivot
                                      value)
-                (handQuickSortHelper (cdr h) 
+                (quickSortHelper (cdr h) 
                                      lesser 
                                      (cons front greater) 
                                      pivot
                                      value))
-            (if (equal value "suit") ;TODO sort cards in here by rank as well
+            (if (equal value "suit") ;TODO sort cards in here by value as well
                 (if (<= (card-suit front) (card-suit pivot))
-                                    (handQuickSortHelper (cdr h) 
-                                     (cons front lesser) 
-                                     greater 
-                                     pivot
-                                     value)
-                (handQuickSortHelper (cdr h) 
-                                     lesser 
-                                     (cons front greater) 
-                                     pivot
-                                     value))
-                nil)))))
+                    (quickSortHelper (cdr h) 
+                                         (cons front lesser) 
+                                         greater 
+                                         pivot
+                                         value)
+                    (quickSortHelper (cdr h) 
+                                         lesser 
+                                         (cons front greater) 
+                                         pivot
+                                         value))
+                (if (equal value "hands")
+                    (if (<= (car(hand-handRank front)) (car(hand-handRank pivot)))
+                        (quickSortHelper (cdr h)
+                                             (cons front lesser)
+                                             greater
+                                             pivot
+                                             value)
+                        (quickSortHelper (cdr h)
+                                             lesser
+                                             (cons front greater)
+                                             pivot
+                                             value))
+                    nil))))))
 
-(defun handQuickSort (h value)
+(defun quickSort (h value)
   (if (or (not(consp  h)) (<= (length h) 1))
       h
       (let* ((lesser '())
              (greater '())
              (pivotIndex (ceiling (length h) 2))
              (pivot (Nth pivotIndex h))
-             (landg (reverse (handQuickSortHelper 
+             (landg (reverse (quickSortHelper 
                               (remove-equal pivot h) 
                               lesser 
                               greater 
                               pivot 
                               value))))
-        (append (handQuickSort(first landg) value)
+        (append (quickSort(first landg) value)
                 (list pivot)
-                (handQuickSort(second landg) value)))))
+                (quickSort(second landg) value)))))
 
 (defun isHighCard (h)
-  (mv 1 (card-rank (car h))))
+  (mv 1 (card-value (car h))))
 
 (defun isPair (h)
   (if (not (consp (cdr h)))
       nil
-      (if (equal (card-rank (car h)) (card-rank (cadr h)))
-          (mv 2 (card-rank (car h)))
+      (if (equal (card-value (car h)) (card-value (cadr h)))
+          (mv 2 (card-value (car h)))
           (isPair (cdr h)))))
 
 (defun isTwoPair (h)
@@ -66,23 +78,23 @@
       (let* ((firstPair (isPair (list (first h) (second h))))
              (secondPair (isPair (cddr h))))
         (if(and(isPair (list (first h) (second h))) (isPair(cddr h)))
-           (mv 3 (card-rank (car h)) (cadr secondPair))
+           (mv 3 (card-value (car h)) (cadr secondPair))
            (isTwoPair (cdr h))))))
 
 (defun isThreeKind (h)
   (if (not (consp (cddr h)))
       nil
       (let* ((firstCard (first h))
-             (firstCardRank (card-rank firstCard))
+             (firstCardRank (card-value firstCard))
              (secondCard (second h))
              (thirdCard (third h)))
-        (if (and (equal firstCardRank (card-rank secondCard))
-                 (equal firstCardRank (card-rank thirdCard)))
+        (if (and (equal firstCardRank (card-value secondCard))
+                 (equal firstCardRank (card-value thirdCard)))
             (mv 4 firstCardRank)
             (isThreeKind (cdr h))))))
 
 (defun isFlush (h)
-  (let* ((sortedBySuit (handQuickSort h "suit"))
+  (let* ((sortedBySuit (quickSort h "suit"))
          (firstCard (first sortedBySuit))
          (firstCardSuit (card-suit firstCard))
          (secondCard (second sortedBySuit))
@@ -95,14 +107,14 @@
                  (equal firstCardSuit (card-suit thirdCard))
                  (equal firstCardSuit (card-suit fourthCard))
                  (equal firstCardSuit (card-suit fifthCard)))
-            (mv 5 (card-rank firstCard)) ;TODO will need better tie breaking figures
+            (mv 5 (card-value firstCard)) ;TODO will need better tie breaking figures
             (isFlush (cdr sortedBySuit))))))
 
 (defun isStraight (h)
   (if (not (consp (cddddr h)))
       nil
-      (let* ((highCardValue (card-rank (first h)))
-             (lowCardValue (card-rank (fifth h))))
+      (let* ((highCardValue (card-value (first h)))
+             (lowCardValue (card-value (fifth h))))
         (if (equal 4 (- highCardValue lowCardValue))
             (mv 6 highCardValue)
             (isStraight (cdr h))))))
@@ -124,43 +136,95 @@
   (if (not (consp (cdddr h)))
       nil
       (let* ((firstCard (first h))
-             (firstCardRank (card-rank firstCard))
+             (firstCardRank (card-value firstCard))
              (secondCard (second h))
              (thirdCard (third h))
              (fourthCard (fourth h)))
-        (if (and (equal firstCardRank (card-rank secondCard))
-                 (equal firstCardRank (card-rank thirdCard))
-                 (equal firstCardRank (card-rank fourthCard)))
+        (if (and (equal firstCardRank (card-value secondCard))
+                 (equal firstCardRank (card-value thirdCard))
+                 (equal firstCardRank (card-value fourthCard)))
             (mv 8 firstCardRank)
             (isFourKind (cdr h))))))
 
 (defun isStraightFlush (h)
   (if (not (consp (cddddr h)))
       nil
-      (let* ((sortedBySuit (handQuickSort h "suit"))
+      (let* ((sortedBySuit (quickSort h "suit"))
              (firstCard (first sortedBySuit))
              (secondCard (second sortedBySuit))
              (thirdCard (third sortedBySuit))
              (fourthCard (fourth sortedBySuit))
              (fifthCard (fifth sortedBySuit))
-             (fiveCardHand (handQuickSort(list firstCard secondCard
+             (fiveCardHand (quickSort(list firstCard secondCard
                                                thirdCard fourthCard
                                                fifthCard)
-                                         "rank")))
+                                         "value")))
         (if (and (isFlush fiveCardHand) (isStraight fiveCardHand))
-            (mv 9 (card-rank (first fiveCardHand)))
+            (mv 9 (card-value (first fiveCardHand)))
             (isStraightFlush (cdr h))))))
+;(defun isGameOver (gamestate))
+;(defun isHandOver (gamestate))
+;(defun rankHands (hands))
 
-(defconst *tester* (hand (handQuickSort (list *D12* *D9* *D5* *D4* *D3* *D2* *D1*) "rank") 1))
-*tester*
-(handQuickSort (hand-cards *tester*) "suit")
+(defun getHandRank (hand)
+  (let* ((straightFlush (isStraightFlush hand))
+         (fourKind (isFourKind hand))
+         (fullHouse (isFullHouse hand))
+         (straight (isStraight hand))
+         (flush (isFlush hand))
+         (threeKind (isThreeKind hand))
+         (twoPair (isTwoPair hand))
+         (pair (isPair hand))
+         (highCard (isHighCard hand)))
+    (if straightFlush
+        straightFlush
+        (if fourKind
+            fourKind
+            (if fullHouse
+                fullHouse
+                (if straight
+                    straight
+                    (if flush
+                        flush
+                        (if threeKind
+                            threeKind
+                            (if twoPair
+                                twoPair
+                                (if pair
+                                    pair
+                                    highCard))))))))))
 
-(isHighCard (hand-cards *tester*))
-(isPair (hand-cards *tester*))
-(isTwoPair (hand-cards *tester*))
-(isThreeKind (hand-cards *tester*))
-(isFlush (hand-cards *tester*))
-(isStraight (hand-cards *tester*))
-(isFullHouse (hand-cards *tester*))
-(isFourKind (hand-cards *tester*))
-(isStraightFlush (hand-cards *tester*))
+(defconst *tester* 
+  (let* ((cards (quickSort (list *D12* *S9* *C5* *H4* *D3* *D2* *D1*) "value"))
+         (handRank (getHandRank cards)))
+    (hand cards handRank)))
+(defconst *tester2* 
+  (let* ((cards (quickSort (list *D12* *C9* *D5* *H4* *D11* *C2* *D1*) "value"))
+         (handRank (getHandRank cards)))
+    (hand cards handRank)))
+(defconst *tester3* 
+  (let* ((cards (quickSort (list *H12* *D9* *D5* *H4* *D8* *D2* *D1*) "value"))
+         (handRank (getHandRank cards)))
+    (hand cards handRank)))
+(defconst *tester4* 
+  (let* ((cards (quickSort (list *D12* *H9* *D5* *H4* *C12* *D2* *H1*) "value"))
+         (handRank (getHandRank cards)))
+    (hand cards handRank)))
+;*tester*
+;*tester2*
+;*tester3*
+;*tester4*
+
+(quickSort (list *tester* *tester2* *tester3* *tester4*) "hands")
+;(quickSort (hand-cards *tester*) "suit")
+
+;(isHighCard (hand-cards *tester*))
+;(isPair (hand-cards *tester*))
+;(isTwoPair (hand-cards *tester*))
+;(isThreeKind (hand-cards *tester*))
+;(isFlush (hand-cards *tester*))
+;(isStraight (hand-cards *tester*))
+;(isFullHouse (hand-cards *tester*))
+;(isFourKind (hand-cards *tester*))
+;(isStraightFlush (hand-cards *tester*))
+;(getHandRank (hand-cards *tester*))
--- structs.lisp
+++ structs.lisp
@@ -2,34 +2,34 @@
 (include-book "world" :dir :teachpacks)
 
 (defstructure card
-	         (suit (:assert (integerp suit)))
-		    (value	(:assert (integerp value))))
+  (suit (:assert (integerp suit)))
+  (value (:assert (integerp value))))
 
 (defstructure hand
-	         (cards (:assert (listp cards))))
+  (cards (:assert (listp cards)))
+  (handRank (:assert (listp handRank))))
 
 (defstructure deck
-   			(cards (:assert (listp cards))))
+  (cards (:assert (listp cards))))
 
 (defstructure player
-	        (name	(:assert (stringp name)))
-		  (chips (:assert (integerp chips)))
-		    (call-amount	(:assert (integerp call-amount)))
-		      (ready (:assert (booleanp ready)))
-		        (cards	(:assert (hand-p cards))))
+  (name (:assert (stringp name)))
+  (chips (:assert (integerp chips)))
+  (ready (:assert (booleanp ready)))
+  (cards (:assert (hand-p cards))))
 
 (defstructure gamestate
-	         (players	(:assert (listp players)))
-		    (common	(:assert (hand-p common)))
-		       (last-raise	(:assert (player-p last-raise)))
-		          (seed	(:assert (integerp seed)))
-			     (pot	(:assert (integerp pot)))
-			(deck (:assert (deck-p deck))))
+  (players (:assert (listp players)))
+  (common (:assert (hand-p common)))
+  (last-raise (:assert (player-p last-raise)))
+  (seed (:assert (integerp seed)))
+  (pot (:assert (integerp pot)))
+  (deck (:assert (deck-p deck))))
 
 (defstructure request
-	         (type (:assert (stringp type)))
-		    (player (:assert (stringp player)))
-		       (bet (:assert (integerp bet))))
+  (type (:assert (stringp type)))
+  (player (:assert (stringp player)))
+  (bet (:assert (integerp bet))))
 
 (defstructure response-other-player
    	(name  (:assert (stringp name)))
